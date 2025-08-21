84346359d068aab9b3c4ed1dc010160dc9f37cc4
JujuXu <jujuxupro@gmail.com>
2025-08-21 17:11:48 +0200

    nice first display

--- JWatch32_waveshare/JWatch32_waveshare.ino
+++ JWatch32_waveshare/JWatch32_waveshare.ino
@@ -4,19 +4,27 @@
 #include <Wire.h>
 #include "SensorPCF85063.hpp"
 
+#include "HWCDC.h"
+HWCDC USBSerial;
+
 Arduino_DataBus *bus = new Arduino_ESP32SPI(LCD_DC, LCD_CS, LCD_SCK, LCD_MOSI);
 
 Arduino_GFX *gfx = new Arduino_ST7789(bus, LCD_RST /* RST */,0 /* rotation */, true /* IPS */, LCD_WIDTH, LCD_HEIGHT, 0, 20, 0, 0);
 
 SensorPCF85063 rtc;
 uint32_t lastMillis;
-char prevHourStr[2] = "";
-char prevMinStr[2] = "";
-char prevSecStr[2] = "";
+char prevHourStr[4] = "";
+char prevMinStr[4] = "";
+char prevSecStr[4] = "";
 char prevDateStr[20] = "";
 
+int16_t getCenteredX(const char *text, uint8_t textSize) {
+  int16_t textWidth = strlen(text) * 6 * textSize;
+  return (LCD_WIDTH - textWidth) / 2;
+}
+
 void setup(void) {
-  Serial.begin(115200);
+  USBSerial.begin(115200);
   Wire.begin(IIC_SDA, IIC_SCL);
 
   if (!rtc.begin(Wire, PCF85063_SLAVE_ADDRESS, IIC_SDA, IIC_SCL)) {
@@ -100,41 +108,41 @@ void loop() {
     sprintf(dateStr, "%02d %02d %04d",datetime.day,datetime.month,datetime.year);
     
     if (strcmp(hourStr, prevHourStr) != 0) {
-      gfx->fillRect(0, 0, LCD_WIDTH, 100, RED);
+      gfx->fillRect(0, 0, LCD_WIDTH, 130, BLACK);
       gfx->setTextColor(CYAN);
-      gfx->setTextSize(10,10,0);
-      gfx->setCursor(0, 0);
+      gfx->setTextSize(13,13,0);
+      gfx->setCursor(getCenteredX(hourStr,13)+6, 10);
       gfx->print(hourStr);
 
       strcpy(prevHourStr, hourStr);
     }
 
     if (strcmp(minStr, prevMinStr) != 0) {
-      gfx->fillRect(0, 110, LCD_WIDTH, 100, GREEN);
+      gfx->fillRect(0, 120, LCD_WIDTH, 100, BLACK);
       gfx->setTextColor(CYAN);
-      gfx->setTextSize(10,10,0);
-      gfx->setCursor(0, 110);
+      gfx->setTextSize(13,13,0);
+      gfx->setCursor(getCenteredX(minStr,13)+6, 120);
       gfx->print(minStr);
 
       strcpy(prevMinStr, minStr);
     }
 
-    /*if (strcmp(secStr, prevSecStr) != 0) {
-      gfx->fillRect(0, 20, LCD_WIDTH, 10, BLACK);
-      gfx->setTextColor(CYAN);
-      gfx->setTextSize(10,10,0);
-      gfx->setCursor(0, 20);
+    if (strcmp(secStr, prevSecStr) != 0) {
+      gfx->fillRect(LCD_WIDTH-40, 190, 40, 21, BLACK);
+      gfx->setTextColor(RED);
+      gfx->setTextSize(3,3,0);
+      gfx->setCursor(LCD_WIDTH-40, 190);
       gfx->print(secStr);
 
       strcpy(prevSecStr, secStr);
-    }*/
+    }
 
     if (strcmp(dateStr, prevDateStr) != 0) {
-      gfx->fillRect(0, 260, LCD_WIDTH, 4, BLACK);
+      gfx->fillRect(0, 260, LCD_WIDTH, 20, BLACK);
       gfx->setTextColor(CYAN);
       gfx->setTextSize(2,2,0);
 
-      gfx->setCursor(0, 260);
+      gfx->setCursor(getCenteredX(dateStr,2), 260);
       gfx->print(dateStr);
       strcpy(prevDateStr, dateStr);
     }
